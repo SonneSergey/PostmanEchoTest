0ee902754afb023acb19925ddbc5f14dbb561efa
Sergey <priestsitnikovsergey@gmail.com>
2025-02-03 21:50:14 +0300

    Добавлены необходимые файлы и папки, добавлен Test в PostmanTest

---
+++ src/test/java/ru/netology/postman/PostmanTest.java
@@ -0,0 +1,24 @@
+package ru.netology.postman;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanTest {
+    @Test
+    void shouldReturnPostTest() {
+
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
+}
